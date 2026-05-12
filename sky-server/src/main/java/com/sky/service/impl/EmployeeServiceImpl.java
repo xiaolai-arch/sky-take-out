d30c6109567c784ba7f91052d19171d011660bac
xiaolai <3419199258@qq.com>
2026-05-12 17:58:29 +0800

    新增员工

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -2,6 +2,7 @@ package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
@@ -9,9 +10,14 @@ import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
+
+import static com.sky.constant.PasswordConstant.DEFAULT_PASSWORD;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -53,6 +59,39 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         //3、返回实体对象
         return employee;
+    }
+
+    /**
+     * 新增员工方法
+     * @param employeeDTO
+     * @return
+     * */
+    public void save(EmployeeDTO employeeDTO){
+        Employee employee = new Employee();
+
+        // 对象的属性的拷贝,从前面拷贝到后面
+        // 属性名称需要一致
+        BeanUtils.copyProperties(employeeDTO, employee);
+
+        // 设置账号状态
+        employee.setStatus(StatusConstant.ENABLE);
+
+        // 设置密码，默认密码
+        employee.setPassword(DigestUtils.md5DigestAsHex(DEFAULT_PASSWORD.getBytes()));
+
+        // 创建时间
+        employee.setCreateTime(LocalDateTime.now());
+
+        // 修改时间
+        employee.setUpdateTime(LocalDateTime.now());
+
+        // 设置创建人的ID
+        // TODO 后期需要改为当前登录用户的ID
+        employee.setCreateUser(10L);
+        employee.setUpdateUser(10L);
+
+        //
+        employeeMapper.insert(employee);
     }
 
 }
